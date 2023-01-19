f85c1f265ecb169321d61db5b6db6600af250464
cassiewatsonn <121472771+cassiewatsonn@users.noreply.github.com>
2023-01-19 16:19:43 -0500

    Added variables for password to .js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,44 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+//Lowercase, uppercase, numbers and special character variables//
+let lowercase= "abcdefghijklmnopqrstuvwxyz"; 
+let uppercase= "abcdefghijklmnopqrstuvwxyz"; 
+let number= "1234567890"; 
+let special= "!@#$%^&*()_-+=?/.,><[]{}~"; 
+
+// let chooselength= number;
+// let chooselowercase= lowercase;
+// let chooseuppercase= uppercase;
+// let choosespecial= special;
+
+
+
+// PASSWORD CRITERIA PROMPTS //
+//Length of Password? 8-128 characters //
+let chooselength = prompt("How many characters would you like to include (minimum: 8, maximum: 128) "); 
+  if (chooselength < 8 || chooselength >128) {
+    alert("Password must be between 8-128 characters");
+  } 
+  //   else if (isNaN(chooselength)){
+  //   alert ("You must enter a number between 8-128");
+  // }
+//Include lowercase, uppercase, numeric, and/or special characters?//
+let chooselowercase = prompt("Would you like to include lowercase letters?");
+let chooseuppercase = prompt("Would you like to include uppercase letters?"); 
+let choosenumber = prompt("Would you like to include numbers?"); 
+let choosespecial = prompt("Would you like to include special characters?"); 
+
+
+function generatePassword(){
+  // chooselength();
+  // chooselowercase(); 
+  // chooseuppercase();
+  // choosenumber();
+  // choosespecial();
+
+}
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +48,10 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
